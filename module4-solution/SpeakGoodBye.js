fc0097b6892b2280c3bf68d88c82d465e6074bc6
Aftab0012 <aftab.mulani1515@gmail.com>
2020-12-18 21:13:46 +0530

    updated

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,5 @@
+var jgreeter = {};
+jgreeter.names =["yaakov", "john" , "joe"]
+for (var i=0,i<names.length,i++){
+console.log("hi:"+names[i]);
+}
---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,6 @@
+var ggreeter = {};
+ggreeter.names =["yaakov", "john" , "joe"]
+for (var i=0,i<names.length,i++){
+console.log("hi:"+names[i]);
+}
+
